afc2601eacc6853b96f284b72e097329065247d1
root <root@DESKTOP-G16VGT5.localdomain>
2022-08-21 14:31:04 +0800

    Revert "-"
    This reverts commit 2647472d3ec06db80f9e813b9cbc89efd77ce4a8.

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -17,13 +17,7 @@
 
 ;; 清空evil insert模式的map,使用默认
 (setcdr evil-insert-state-map nil)
-;; escape
-(define-key evil-normal-state-map (kbd "<escape>") 'evil-normal-state)
-(define-key evil-insert-state-map (kbd "<escape>") 'evil-normal-state)
-(define-key evil-replace-state-map (kbd "<escape>") 'evil-normal-state)
-(define-key evil-visual-state-map (kbd "<escape>") 'evil-normal-state)
-(define-key evil-motion-state-map (kbd "<escape>") 'evil-normal-state)
-(define-key evil-operator-state-map (kbd "<escape>") 'evil-normal-state)
+(define-key evil-insert-state-map [escape] 'evil-normal-state)
 
 
 (defun evil-file()
