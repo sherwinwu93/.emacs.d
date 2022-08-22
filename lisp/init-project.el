b682d1e48b45ebdf177335edc559b90f19b4f946
root <root@wusd.localdomain>
2022-08-22 17:37:30 +0800

    -

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -1,17 +1,17 @@
 ;; ~/.emacs.d/init.el
 ;; ----------------------------------------project
 (use-package projectile
+  :ensure t
+  :bind (("C-c p" . projectile-command-map))
   :config
-  ;; 把它的缓存挪到 ~/.emacs.d/.cache/ 文件夹下，让 gitignore 好做
-  (setq projectile-cache-file (expand-file-name ".cache/projectile.cache" user-emacs-directory))
-  ;; 全局 enable 这个 minor mode
-  (projectile-mode 1)
-  (evil-leader/set-key "p" 'projectile-command-map)
-  )
+  (setq projectile-mode-line "Projectile")
+  (setq projectile-track-known-projects-automatically nil))
+
 (use-package counsel-projectile
   :ensure t
   :after (projectile)
   :init (counsel-projectile-mode))
+(evil-leader/set-key "p" 'projectile-command-map)
 
 ;; ----------------------------------------neotree
 (use-package neotree
