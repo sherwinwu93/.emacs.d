5600b37e934b538767447896de2511ac4d776c96
root <root@DESKTOP-G16VGT5.localdomain>
2022-08-25 20:45:50 +0800

    -

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -5,7 +5,7 @@
   :bind (("C-c p" . projectile-command-map))
   :config
   (setq projectile-mode-line "Projectile")
-  (setq projectile-track-known-projects-automatically nil))
+  )
 
 (use-package counsel-projectile
   :ensure t
