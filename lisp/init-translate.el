85e0d1bf56407e86f72839c4578a4c8cb2b32b76
root <root@lenovo.localdomain>
2023-08-18 11:26:07 +0800

    -

--- lisp/init-translate.el
+++ lisp/init-translate.el
@@ -7,23 +7,30 @@
 ;; (fset 'wusd/add-pronunciation
 ;;    (kmacro-lambda-form [escape ?j ?0 ?v ?e f7 ?  tab ?y ?$ ?  tab ?p] 0 "%d"))
 
-(use-package bing-dict
-  :load-path "~/.emacs.d/lisp/extensions"
-  :init
-  ;; (setq bing-dict-cache-auto-save t)
-  )
-(defun show-dict-result-in-other-window()
-  (interactive)
-	(switch-to-buffer-other-window "*wusd*")
-  (erase-buffer)
-	(insert wusd/dict-result)
-  (evil-beginning-of-line)
-  (select-window-1))
+;; (use-package bing-dict
+;;   :load-path "~/.emacs.d/lisp/extensions"
+;;   :init
+;;   ;; (setq bing-dict-cache-auto-save t)
+;;   )
+;; (defun show-dict-result-in-other-window()
+;;   (interactive)
+;; 	(switch-to-buffer-other-window "*wusd*")
+;;   (erase-buffer)
+;; 	(insert wusd/dict-result)
+;;   (evil-beginning-of-line)
+;;   (select-window-1))
 
-(fset 'wusd/add-pronunciation
-   (kmacro-lambda-form [escape f10 ?0 ?v ?e ?  tab ?v ?E ?E ?h ?y ?  tab ?p ?j ?0] 0 "%d"))
-(fset 'wusd/bing-dict-brief
-   (kmacro-lambda-form [f7 return] 0 "%d"))
+;; (fset 'wusd/add-pronunciation
+;;    (kmacro-lambda-form [escape f10 ?0 ?v ?e ?  tab ?v ?E ?E ?h ?y ?  tab ?p ?j ?0] 0 "%d"))
+;; (fset 'wusd/bing-dict-brief
+;;    (kmacro-lambda-form [f7 return] 0 "%d"))
+
+;; If you want English-English dictionary only.
+(use-package fanyi
+  :ensure t
+  :custom
+  (fanyi-providers '(fanyi-etymon-provider
+                     fanyi-longman-provider)))
 
 (provide 'init-translate)
 
